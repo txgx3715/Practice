f65e880efc1a76acbdb86ca3038cd456cd8be39d
Mobacola <68771516+txgx3715@users.noreply.github.com>
2020-08-07 23:36:38 +0800

    Add files via upload

---
+++ Classes&Objects .py
@@ -0,0 +1,14 @@
+#In Python we have three type of data
+#Number,String and Boolean Value.
+#We can use Classes and Objects to create belong to us speical type of data.
+
+
+#Now we define a students classes.
+
+class Students:
+
+def __init__(self, name, major, gpa, is_on_probition):
+    self.name = name
+    self.major = major
+    self.gpa = gpa
+    self.is_on_probition = is_on_probition
